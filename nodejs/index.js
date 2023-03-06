bf1f42f9502e91628deda07d7ca98f9e9ddf17c8
rubgonama <53659854+Irtharan@users.noreply.github.com>
2023-03-06 15:15:47 +0100

    Añadido RGA

--- nodejs/index.js
+++ nodejs/index.js
@@ -246,3 +246,34 @@ app.get("/samples/TTG",(req,res)=>{
     // Mensaje por pantalla
     res.send("La media de los viajeros en Alemania es de: "+media.toString());
 });
+
+app.get("/samples/TTG",(req,res)=>{
+    ar datos = [
+        { territory: "andalucia", year: 2008, telecommunication: 2328527, computer_program_edition: 19.662 programming_and_others: 455372 },
+        { territory: "andalucia", year: 2009, telecommunication: 2208025, computer_program_edition: 16803, programming_and_others: 555635 },
+        { territory: "andalucia", year: 2010, telecommunication: 2043994, computer_program_edition: 16404, programming_and_others: 576863 },
+        { territory: "andalucia", year: 2011, telecommunication: 1922890, computer_program_edition: 15181, programming_and_others: 571181 },
+        { territory: "andalucia", year: 2012, telecommunication: 1696716, computer_program_edition: 29119, programming_and_others: 507228 },
+        { territory: "spain", year: 2008, telecommunication: 18579368, computer_program_edition: 458703, programming_and_others: 10474035 },
+        { territory: "spain", year: 2009, telecommunication: 18325916, computer_program_edition: 464724, programming_and_others: 10617023 },
+        { territory: "spain", year: 2010, telecommunication: 16984206, computer_program_edition: 457495, programming_and_others: 10822351 },
+        { territory: "spain", year: 2011, telecommunication: 16306440, computer_program_edition: 464939, programming_and_others: 11115619 },
+        { territory: "spain", year: 2012, telecommunication: 14888620, computer_program_edition: 431462, programming_and_others: 11419614 }
+    ];
+
+    // Datos de territorio de España
+    var valores = datos.filter(territorio => territorio.territory == "spain");
+
+    // Valores de telecommunication
+    var valores_traveler = [];
+    valores.forEach(valor => {valores_traveler.push(valor.telecommunication)});
+
+    //calcular media
+    suma_total = 0;
+    valores_traveler.forEach(num => {suma_total+= num});
+    media = Math.round((suma/valores_traveler.length) * 1000) / 1000;
+    
+
+    // Mensaje por pantalla
+    res.send("La media de la inversión en programación enEspaña es de: "+media.toString());
+});
