0407f192eeaa09148ee2e65b996eb784cd11234a
Antonio Carranza Barroso <49164757+carran97@users.noreply.github.com>
2023-03-27 01:57:38 +0200

    Codigo modularizado v2

--- nodejs/index.js
+++ nodejs/index.js
@@ -10,8 +10,9 @@ const BASE_API_URL = "/api/v1";
 
 app.use("/",express.static("./public"));
 
-app.use(bodyParser.json()); //PARSEA a JSON DIRECTAMENTE
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(port,()=>{
     console.log(`Listening in port ${port}`);
 });
