888e4ed4be85c982ca51340ab612a8708c3e3dc7
Antonio Carranza Barroso <49164757+carran97@users.noreply.github.com>
2023-05-22 04:17:03 +0200

    Vuelta version

--- e2e/ict.spec.js
+++ e2e/ict.spec.js
@@ -3,33 +3,41 @@ import { test, expect } from '@playwright/test';
 test('carga recursos iniciales', async ({ page }) => {
   await page.goto('https://sos2223-22.appspot.com/frontCgm');
 
-  
-  await page.click('button:has-text("Cargar recursos")');
   await page.waitForTimeout(2000);
+  await page.click('button:has-text("Cargar recursos")');
+
   const mensajeExito = page.locator('.alert');
   await expect(mensajeExito).toHaveText(' Recurso/s cargado/s correctamente');
 });
 
-test('realiza una búsqueda', async ({ page }) => {
-  await page.goto('https://sos2223-22.appspot.com/frontCgm');
-  await page.click('button:has-text(" Buscar recursos")');
+// test('realiza una búsqueda', async ({ page }) => {
+//   await page.goto('https://sos2223-22.appspot.com/frontCgm');
+//   await page.click('button:has-text("Buscar recursos")');
+//   await page.waitForTimeout(2000);
+//   await page.fill('input[placeholder=" Territorio"]', '');
+//   await page.fill('input[placeholder="Año"]', '2002221');
+//   await page.fill('input[placeholder="industria de fabricación de TIC"]', '');
+//   await page.fill('input[placeholder="Comercio al por mayor"]', '');
+//   await page.fill('input[placeholder="Programa de computadora de edición"]', '');
 
-  await page.fill('input[placeholder=" Territorio"]', '');
-  await page.fill('input[placeholder="Año"]', '2021');
-  await page.fill('input[placeholder="industria de fabricación de TIC"]', '');
-  await page.fill('input[placeholder="Comercio al por mayor"]', '');
-  await page.fill('input[placeholder="Programa de computadora de edición"]', '');
-
-  await page.click('button:has-text(" Búsqueda")');
-  await page.waitForTimeout(2000);
-
-  const mensajeExito = await page.$('td'); // Agregado el await para esperar a que se encuentre el elemento
-  const textoMensaje = await (await mensajeExito.getProperty('innerText')).jsonValue(); // Obtener el texto del elemento
-  expect(textoMensaje).toContain('spain');
+//   await page.click('button:has-text(" Buscar")');
+//   const pageContent = await page.textContent('body');
+//   // Verificar que el mensaje no esté presente en el contenido de la página
+//   await expect(pageContent).not.toContain('No existen datos para mostrar');
+// });
 
 
-});
 
+// test('borra un recurso', async ({ page }) => {
+//   await page.goto('https://sos2223-22.appspot.com/frontCgm');
+
+//   await page.waitForTimeout(2000);
+//   await page.click('button:has-text("Borrar"):only-child');
+//  // const button = await page.$('button:has-text("Borrar"):only-child');
+
+//   const mensajeExito = page.locator('#messages');
+//   await expect(mensajeExito).toHaveText('borrado correctamente');
+// });
 
 test('borra todos los recursos', async ({ page }) => {
   await page.goto('https://sos2223-22.appspot.com/frontCgm');
